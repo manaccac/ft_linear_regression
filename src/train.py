6cbc2f80780f7eff51ec2ec890fb5674d25d55c0
manaccac <maximenaccache@gmail.com>
2023-08-22 10:44:34 +0200

    je pense que c'est bon

--- src/train.py
+++ src/train.py
@@ -27,6 +27,13 @@ if len(data) == 0:
 
 kilometrage = data['km'].values
 prices = data['price'].values
+len_prices = len(prices)
+
+# Calcul de la moyenne des prix
+mean_prices = sum(prices) / len_prices
+
+# Calcul de la somme totale des carrés
+sst = sum((price - mean_prices)**2 for price in prices)
 
 # Data normalisation
 # moyenne
@@ -37,11 +44,12 @@ kilometrage_std = (sum((mi - kilometrage_mean) ** 2 for mi in kilometrage) / len
 # normalisation
 kilometrage_normalized = [(mi - kilometrage_mean) / kilometrage_std for mi in kilometrage]
 
+
+cost_history = []
 theta0 = 0
 theta1 = 0
-learning_rate = 0.001
-iterations = 10000
-len_prices = len(prices)
+learning_rate = 0.005
+iterations = 2000
 
 def estimate_price(kilometrage):
     return theta0 + theta1 * kilometrage
@@ -66,11 +74,20 @@ for y in range(iterations):
     
     # calcule de cout ca mesure à quel point le modèle s'adapte aux données.
     cost = sum([(estimate_price(kilometrage_normalized[i]) - prices[i])**2 for i in range(len_prices)]) / (2*len_prices)
+    cost_history.append(cost)
     if cost >= previous_cost:
-		# problème avec le taux d'apprentissage
+        # problème avec le taux d'apprentissage
         print("Gradient descent does not converge!")
         exit()
     previous_cost = cost
+
+# Calcul de la somme des carrés des erreurs
+sse = sum((estimate_price(kilometrage_normalized[i]) - prices[i])**2 for i in range(len_prices))
+
+# Calcul du coefficient de détermination R^2
+r2_score = 1 - (sse / sst)
+r2_score = 100 * r2_score
+print(f"La precision de l'algorithme est de:  {r2_score}")
 
 with open('model_params/parameters.txt', 'w') as f:
     f.write(f"{theta0}\n")
@@ -78,10 +95,30 @@ with open('model_params/parameters.txt', 'w') as f:
     f.write(f"{kilometrage_mean}\n")
     f.write(f"{kilometrage_std}\n")
 
-# Data visualisation
+# Figure pour les trois subplots
+plt.figure(figsize=(15, 5))
+
+plt.subplot(1, 3, 1)
 plt.scatter(kilometrage, prices, color='blue', label='Data points')
-plt.plot(kilometrage, [estimate_price((x - kilometrage_mean) / kilometrage_std) for x in kilometrage], color='red', label='Regression line')
-plt.xlabel('kilometrage')
+plt.xlabel('Kilometrage')
 plt.ylabel('Price')
 plt.legend()
+
+plt.subplot(1, 3, 2)
+plt.scatter(kilometrage, prices, color='blue', label='Data points')
+plt.plot(kilometrage, [estimate_price((x - kilometrage_mean) / kilometrage_std) for x in kilometrage], color='red', label='Regression line')
+plt.xlabel('Kilometrage')
+plt.ylabel('Price')
+plt.text(0.05, 0.95, f'Précision: {r2_score:.2f}%', transform=plt.gca().transAxes, verticalalignment='top')
+plt.legend()
+
+
+# Subplot 3: Courbe d'apprentissage
+plt.subplot(1, 3, 3)
+plt.plot(cost_history)
+plt.title('Courbe d\'apprentissage')
+plt.xlabel('Itérations')
+plt.ylabel('Coût')
+
+# Afficher la figure avec les trois subplots
 plt.show()
